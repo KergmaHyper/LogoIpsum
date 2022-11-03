5e921c809da88e5c909f926337b526364a7fe3b8
kergma@gmail.com <kergma@gmail.com>
2022-11-03 13:25:12 +0200

    november

---
+++ index.jsx
@@ -0,0 +1,24 @@
+const rootNode = document.getElementById("app");
+const root = ReactDOM.createRoot(rootNode);
+
+
+const Map = function (props) {
+    return (<div class="container"><div id={props.mapId}></div></div>);
+};
+
+root.render(
+    <Map mapId="map" />
+);
+
+function ticTime() {
+    const timeD = ReactDOM.createRoot(document.getElementById("timeD"));
+    timeD.render(
+        <CurrTime />
+    );
+};
+const CurrTime = () => {
+    return (<h2>current time {new Date().toLocaleTimeString()}</h2>);
+};
+
+setInterval(ticTime, 1000);
+
