bbeba871b0e3ca8ae902fa854ffb0db33bc6ec28
kergma@gmail.com <kergma@gmail.com>
2022-11-07 03:06:59 +0200

    fix

--- script/initMap.js
+++ script/initMap.js
@@ -1,49 +1,42 @@
 let map;
 let infoWindow;
-let p = { lat: 0, lng: 0 };
 let marker;
-
-// let pos = { lat: 48.93, lng: 38.49 };
 let pos;
-console.log("Init pos: ", pos);
-
-function writePos(pos_) {
-    let lat = document.getElementById('lat');
-    lat.textContent = pos_.lat;
-    let lng = document.getElementById('lng');
-    lng.textContent = pos_.lng;
+let p = {
+    lat: 49,
+    lng: 38,
+    getCoords(ll) { this.lat = ll.lat(); this.lng = ll.lng(); }
 };
 
 
+function writePos(pos) {
+    let lat = document.getElementById('lat');
+    let lng = document.getElementById('lng');
+    lat.textContent = pos.lat;
+    lng.textContent = pos.lng;
+};
+
 function success(position) {
     console.log('start success coord function');
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    var altitude = position.coords.altitude;
-    var speed = position.coords.speed;
-    pos = { lat: latitude, lng: longitude };
-
+    let lat = position.coords.latitude;
+    let lng = position.coords.longitude;
+    pos = { lat, lng };
     console.log("Centering map to coord: ", pos);
 
     infoWindow.setPosition(pos);
     infoWindow.setContent("Location found.");
     infoWindow.open(map);
+
     map.setCenter(pos);
-
-
     console.log("Setting marker to coord: ", pos);
-    var optMarker = {
+    let optMarker = {
         position: pos,
-        map: map,
+        map,
         title: 'You here!',
     };
     marker = new google.maps.Marker(optMarker);
     console.log(optMarker, marker);
-
     writePos(pos);
-
-
-
 };
 
 function error(obj) {
@@ -52,33 +45,25 @@ function error(obj) {
     console.log(pos);
 };
 
-
-function placeMarkerAndPanTo(latLng, map) {
-    new google.maps.Marker({
-        position: latLng,
-        map: map,
-    });
-
-
+function placeMarkerAndPanTo(position, map) {
+    new google.maps.Marker({ position, map, });
     map.panTo(latLng);
-    p = { lat: latLng.lat(), lng: latLng.lng() };
+    p.getCoords(position);
     console.log("p: ", p);
     writePos(p);
 };
 
 function moveMarker(latLng) {
-
-    p = { lat: latLng.lat(), lng: latLng.lng() };
+    marker.setPosition(latLng);
+    p.getCoords(latLng);
     console.log("p: ", p);
     writePos(p);
-    marker.setPosition(latLng);
 }
 
 function initMap() {
     console.log("start initMap");
     console.log("getting loale position");
     navigator.geolocation.getCurrentPosition(success, error);
-
 
     map = new google.maps.Map(document.getElementById("map"), {
         center: pos,
@@ -90,6 +75,7 @@ function initMap() {
         },
     });
     infoWindow = new google.maps.InfoWindow();
+
     map.addListener("click", (e) => {
         // placeMarkerAndPanTo(e.latLng, map);
         moveMarker(e.latLng);
